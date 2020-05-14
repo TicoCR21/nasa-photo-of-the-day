b06a29f126d91563241c54e53cae619f84a648c2
TicoCR16 <mariojsanchez15@gmail.com>
2020-05-13 18:19:43 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import axios from "axios";
+// css files
 import "./App.css";
 
-function App() {
+const photoOfTheDataResponse = axios.get( "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY" )
+                                    .then( response => console.log( response ) );
+
+function App() 
+{
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className = "App" >
+      
     </div>
   );
 }
